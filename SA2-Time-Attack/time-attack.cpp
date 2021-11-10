9e259da2fb3a0b06554847c2e5c683be2e19d4f5
Sora <sora.x@hotmail.fr>
2021-11-10 03:08:23 +0100

    time attack working! Reading information from savefile :D

--- SA2-Time-Attack/time-attack.cpp
+++ SA2-Time-Attack/time-attack.cpp
@@ -2,17 +2,27 @@
 #include "time-attack.h"
 
 Trampoline* CheckAndLoadCountDown_t;
-
+SaveLevelInfo* savePtr = nullptr;
 
 
 void CheckAndLoadCountDown_r(char min, char sec) {
 
 	if (pbRace)
 	{
-		LoadCountDownTimer(3, 0);
-		return;
-	}
+		savePtr = (SaveLevelInfo*)&levelThing[196 * CurrentLevel];
+		int mission = MissionNum;
 
+		if (mission > 0)
+			mission *= 3;
+
+		if (savePtr) {
+
+			if (savePtr->Scores->Time.Minutes > 0 || savePtr->Scores->Time.Seconds > 0) {
+				LoadCountDownTimer(savePtr->Scores[mission].Time.Minutes, savePtr->Scores[mission].Time.Seconds);
+				return;
+			}
+		}
+	}
 
 	VoidFunc(original, CheckAndLoadCountDown_t->Target());
 	original();
--- SA2-Time-Attack/time-attack.h
+++ SA2-Time-Attack/time-attack.h
@@ -2,3 +2,4 @@
 
 VoidFunc(CheckAndLoadCountDown, 0x451F00);
 FunctionPointer(void, LoadCountDownTimer, (char a1, char a2), 0x451EA0);
+DataArray(char, levelThing, 0x1DEC638, 17640); //weird ass struct save used to get player level stats
